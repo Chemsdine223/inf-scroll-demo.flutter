34444054331a3fd7dac2fe6761c7a6b28362a1da
Chemsdine <126870359+Chemsdine223@users.noreply.github.com>
2024-05-10 09:43:04 +0000

    Update main.dart
    This is the modification that allows refresh upon fewer items in the infinite scroll

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:rxdart/rxdart.dart';
+import 'package:flutter/widgets.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -21,29 +21,40 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key}) : super(key: key);
+  const MyHomePage({super.key});
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  late BehaviorSubject<List<int>> _dataSubject;
-  late ScrollController _scrollController;
   final int _perPage = 10;
   int _counter = 0;
+  final List<int> _data = [];
+  double? _height = 100;
+
+  late ScrollController _scrollController;
 
   @override
   void initState() {
     super.initState();
-    _dataSubject = BehaviorSubject<List<int>>();
-    _scrollController = ScrollController()..addListener(_scrollListener);
+    _scrollController = ScrollController(
+      onAttach: (position) {
+        handleAttach();
+      },
+    );
+    _scrollController.addListener(_scrollListener);
     loadData();
+  }
+
+  void handleAttach() {
+    // setState(() {
+    _height = MediaQuery.of(context).size.height / 2;
+    // });
   }
 
   @override
   void dispose() {
-    _dataSubject.close();
     _scrollController.dispose();
     super.dispose();
   }
@@ -51,17 +62,20 @@ class _MyHomePageState extends State<MyHomePage> {
   void loadData() {
     // Simulating loading data asynchronously
     Future.delayed(const Duration(seconds: 2), () {
-      final newData = List.generate(_perPage, (index) => _counter * _perPage + index + 1);
-      _dataSubject.add(newData);
-      _counter++; // Increment counter for pagination
+      final newData =
+          List.generate(_perPage, (index) => _counter * _perPage + index + 1);
+      setState(() {
+        _data.addAll(newData);
+        _counter++;
+      });
     });
   }
 
   void _scrollListener() {
-    if (_scrollController.offset >= _scrollController.position.maxScrollExtent &&
-        !_scrollController.position.outOfRange) {
-      // Reached the end, load more data
+    if (_scrollController.position.pixels >=
+        _scrollController.position.maxScrollExtent) {
       loadData();
+      // _height = null;
     }
   }
 
@@ -71,32 +85,36 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: const Text('Infinite Scroll Example'),
       ),
-      body: StreamBuilder<List<int>>(
-        stream: _dataSubject.stream,
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            final data = snapshot.data!;
-            return ListView.builder(
+      body: Column(
+        children: [
+          Expanded(
+            // height: _height,
+            child: ListView.builder(
+              shrinkWrap: false,
               controller: _scrollController,
-              itemCount: data.length + 1, // +1 for loading indicator
+              itemCount: _data.length + 1,
               itemBuilder: (context, index) {
-                if (index < data.length) {
-                  return ListTile(
-                    title: Text('Item ${data[index]}'),
+                if (index < _data.length) {
+                  return Container(
+                    // color: Colors.red,
+                    child: ListTile(
+                      title: Text('Item ${_data[index]}'),
+                    ),
                   );
                 } else {
-                  return const Center(
-                    child: CircularProgressIndicator(),
+                  return const SizedBox(
+                    height: 200,
+                    // color: Colors.green,
+                    // height: ,
+                    child: Center(
+                      child: CircularProgressIndicator(),
+                    ),
                   );
                 }
               },
-            );
-          } else {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-        },
+            ),
+          ),
+        ],
       ),
     );
   }
